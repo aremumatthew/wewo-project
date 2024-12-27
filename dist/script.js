3704f37ca6053fd91c19fd6a63395e7105708038
aremumatthew <aremumatthew112@gmail.com>
2024-12-27 12:00:12 +0000

    edited the side bar and adding javascript file

---
+++ dist/script.js
@@ -0,0 +1,8 @@
+function openNav(){
+const navElement = document.getElementById('navList');
+ navElement.style.width = '100%';
+}
+function closeNav(){
+ const navElement = document.getElementById('navList');
+ navElement.style.width = '0';
+}
